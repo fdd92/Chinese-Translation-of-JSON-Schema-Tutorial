248066f5fcf674ff1253710ee636cfbc746f8d2b
howie <haia25675@gmail.com>
2019-03-19 17:55:17 +0800

    init gitbook

---
+++ jfet.config.js
@@ -0,0 +1,12 @@
+module.export = {
+    doc() {
+        return {
+            name: 'JSON-SCHEMA-LEARN', // 文档名
+            title: 'JSON-SCHEMA 教程', // 文档标题
+            desc: 'JSON-SCHEMA 官方教程中文翻译',
+            timeout: 10000,
+            token: '21232F297A57A5A743894A0E4A801FC3',
+            uploadUrl: 'http://doc.fe.jyb.com/api/upload'
+        }
+    }
+};
