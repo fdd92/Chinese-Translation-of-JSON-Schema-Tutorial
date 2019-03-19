d041a6bee0a71ba47b18999b0ce9889a3529c2b1
howie <haia25675@gmail.com>
2019-03-19 18:37:14 +0800

    init gitbook

--- jfet.config.js
+++ jfet.config.js
@@ -1,12 +1,16 @@
-module.export = {
+/**
+ * jfet配置
+ */
+
+module.exports = {
     doc() {
         return {
-            name: 'JSON-SCHEMA-LEARN', // 文档名
-            title: 'JSON-SCHEMA 教程', // 文档标题
+            name: 'JSON-SCHEMA-LEARN',
+            title: 'JSON-SCHEMA教程',
             desc: 'JSON-SCHEMA 官方教程中文翻译',
             timeout: 10000,
             token: '21232F297A57A5A743894A0E4A801FC3',
             uploadUrl: 'http://doc.fe.jyb.com/api/upload'
-        }
+        };
     }
 };
